1871e83ec91d824fc27dccb7ff5c6429e3d3f156
Maksim Gamolin <m.gamolin@corp.mail.ru>
2021-10-09 17:28:43 +0300

    Создание датасемпла

---
+++ plant/cases/create_data_sample.py
@@ -0,0 +1,42 @@
+from collections import namedtuple
+
+from plant.models import DataSampling
+from stock.models import Dataset
+
+RawOperations = namedtuple('RawOperations', ['filtering', 'aggregation', 'features'])
+
+
+class CreateDataSampleService:
+
+    def __init__(self, workpiece,
+                 dataset: Dataset,
+                 raw_operations: RawOperations):
+        self._workpiece = workpiece
+        self._dataset = dataset
+        self._raw_operations = raw_operations
+        self._data_sample = None
+
+    def _configure_data_sample(self):
+        self._data_sample = DataSampling.objects.create(
+            workpiece=self._workpiece,
+            dataset=self._dataset,
+            raw_filtering=self._raw_operations.filtering,
+            raw_aggregation=self._raw_operations.aggregation,
+            raw_features=self._raw_operations.features
+        )
+
+    def _raw_filters_to_filters(self):
+        pass
+
+    def _raw_aggregations_to_aggregations(self):
+        pass
+
+    def _raw_features_to_features(self):
+        pass
+
+    def create_data_sample(self):
+        self._configure_data_sample()
+        self._raw_features_to_features()
+        self._raw_aggregations_to_aggregations()
+        self._raw_features_to_features()
+        return self._workpiece
--- plant/urls.py
+++ plant/urls.py
@@ -4,5 +4,6 @@ from plant import views
 
 urlpatterns = [
     path('workpiece/<int:pk>/', views.WorkpieceDetailView.as_view()),
-    path('init-workpiece/', views.InitWorkpieceView.as_view())
+    path('init-workpiece/', views.InitWorkpieceView.as_view()),
+    path('create-data-peace/', views.CreateDataSampleView.as_view())
 ]
